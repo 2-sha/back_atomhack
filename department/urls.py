00a0c740dcaf791a8df5b65d47e100a4610339bb
Антон Халитов <khalitov.anton@yandex.ru>
2021-08-29 09:51:40 +0300

    Урлы

---
+++ department/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .views import DepartmentViewSet
+
+router = DefaultRouter()
+router.register(r'', DepartmentViewSet, basename='department')
+urlpatterns = router.urls
